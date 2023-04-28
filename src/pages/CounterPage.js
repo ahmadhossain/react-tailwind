f008161fba6a25d8465491a5ef83b680ab34856a
Ahmad Hossain <ahmadhossain.bu@gmail.com>
2023-04-28 13:03:39 +0600

    useReducer

--- src/pages/CounterPage.js
+++ src/pages/CounterPage.js
@@ -1,34 +1,72 @@
 import React from "react";
 import Button from "../components/Button";
-import { useState } from "react";
+import { useReducer } from "react";
 
+const INCREMENT_COUNT = "increment";
+const DECREMENT_COUNT = "decrement";
+const SET_VALUE_TO_ADD = "set_value_to_add";
+const ADD_VALUE_TO_COUNT = "add_value_to_count";
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case INCREMENT_COUNT:
+      return {
+        ...state,
+        count: state.count + 1,
+      };
+    case DECREMENT_COUNT:
+      return {
+        ...state,
+        count: state.count - 1,
+      };
+    case SET_VALUE_TO_ADD:
+      return {
+        ...state,
+        valueToAdd: action.payload,
+      };
+    case ADD_VALUE_TO_COUNT:
+      return {
+        ...state,
+        count: state.valueToAdd + state.count,
+        valueToAdd: 0,
+      };
+  }
+};
 const CounterPage = () => {
-  const [count, setCount] = useState(0);
-  const [valueToAdd, setValueToAdd] = useState(0);
+  const [state, dispatch] = useReducer(reducer, { count: 0, valueToAdd: 0 });
+
   const increment = () => {
-    setCount(count + 1);
+    dispatch({
+      type: INCREMENT_COUNT,
+    });
   };
   const decrement = () => {
-    setCount(count - 1);
+    dispatch({
+      type: DECREMENT_COUNT,
+    });
   };
   const haldleChange = (event) => {
     const value = parseInt(event.target.value) || 0;
-    setValueToAdd(value);
+    dispatch({
+      type: SET_VALUE_TO_ADD,
+      payload: value,
+    });
   };
   const handleSubmit = (event) => {
     event.preventDefault();
-    setCount(count + valueToAdd);
-    setValueToAdd(0);
+    dispatch({
+      type: ADD_VALUE_TO_COUNT,
+    });
   };
   return (
     <div className="border max-w-3xl mx-auto mt-24 rounded p-3 shadow bg-white w-full">
-      <h1>Count is {count}</h1>
+      <h1>Count is {state.count}</h1>
       <Button onClick={increment}>Increment</Button>
       <Button onClick={decrement}>Decrement</Button>
       <form onSubmit={handleSubmit}>
         <label>Add a lot</label>
         <input
-          value={valueToAdd || ""}
+          value={state.valueToAdd || ""}
           type="number"
           onChange={haldleChange}
           className="p-1 m-3 border border-gray-300"
